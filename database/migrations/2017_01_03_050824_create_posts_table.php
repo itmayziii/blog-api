1c04ba6cd7e6f107675ae1654036de00b3513ed0
itmayziii <tommymay37@gmail.com>
2018-03-01 00:19:13 -0500

    Updated tables to more accurately reflect data.

--- database/migrations/2017_01_03_050824_create_posts_table.php
+++ database/migrations/2017_01_03_050824_create_posts_table.php
@@ -14,13 +14,15 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->string('slug')->primary();
+            $table->increments('id');
+            $table->string('slug')->unique()->index();
             $table->timestamps();
             $table->integer('user_id')->unsigned();
             $table->integer('category_id')->unsigned();
             $table->string('status', 20)->default('draft');
             $table->string('title', 200)->unique();
             $table->longText('content')->nullable();
+            $table->string('preview')->nullable();
             $table->string('image_path')->nullable();
 
             $table->foreign('user_id')->references('id')->on('users');
