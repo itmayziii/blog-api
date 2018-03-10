1c770b0a747d305ff5f542164fe5734e7605ee2d
itmayziii <tommymay37@gmail.com>
2018-03-09 23:53:52 -0500

    Changed CategoryRepository to only show live posts unless use is an admin

--- app/Http/Controllers/CategoryPostController.php
+++ app/Http/Controllers/CategoryPostController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\JsonApi;
+use App\Post;
 use App\Repositories\CacheRepository;
 use App\Repositories\CategoryRepository;
+use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Http\Response;
 
 class CategoryPostController
@@ -21,27 +23,40 @@ class CategoryPostController
      * @var CacheRepository
      */
     private $cacheRepository;
+    /**
+     * @var Gate
+     */
+    private $gate;
 
-    public function __construct(JsonApi $jsonApi, CategoryRepository $categoryRepository, CacheRepository $cacheRepository)
+    public function __construct(JsonApi $jsonApi, CategoryRepository $categoryRepository, CacheRepository $cacheRepository, Gate $gate)
     {
         $this->jsonApi = $jsonApi;
         $this->categoryRepository = $categoryRepository;
         $this->cacheRepository = $cacheRepository;
+        $this->gate = $gate;
     }
 
     /**
      * Find specific category with all related posts.
      *
      * @param Response $response
+     * @param Post $post
      * @param string $slug
      *
      * @return Response
      */
-    public function show(Response $response, $slug) // TODO figure out how we can paginate included JSON API resources
+    public function show(Response $response, Post $post, $slug) // TODO figure out how we can paginate included JSON API resources
     {
-        $category = $this->cacheRepository->remember("categories-posts.$slug", 60, function () use ($slug) {
-            return $this->categoryRepository->findBySlugWithPosts($slug);
-        });
+        if ($this->gate->denies('indexAllPosts', $post)) {
+            $category = $this->cacheRepository->remember("categories-posts.{$slug}.live", 60, function () use ($slug) {
+                return $this->categoryRepository->findBySlugWithPosts($slug, true);
+            });
+        } else {
+            $category = $this->cacheRepository->remember("categories-posts.{$slug}.all", 60, function () use ($slug) {
+                return $this->categoryRepository->findBySlugWithPosts($slug, false);
+            });
+        }
+
         if (is_null($category)) {
             return $this->jsonApi->respondResourceNotFound($response);
         }
